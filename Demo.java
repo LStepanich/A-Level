b68fb9f08e854724d6d576ddde3d5c1271f6ae07
LStepanich <36239523+LStepanich@users.noreply.github.com>
2018-02-28 21:50:36 +0200

    try-with-recourses

--- Demo.java
+++ Demo.java
@@ -1,31 +1,19 @@
-import java.util.Scanner;
 import java.io.*;
 
 public class Demo {
 	public static String read(String path) {
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(path));
-		} catch (FileNotFoundException e) {
-			reader = null;
-			e.printStackTrace();
-		} 
-		StringBuilder stringBuilder = new StringBuilder();
-		String currentString;
-		try {
+		try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+			StringBuilder stringBuilder = new StringBuilder();
+			String currentString;
 			while ((currentString = reader.readLine()) != null) {
 				stringBuilder.append(currentString);
 				stringBuilder.append("\n");
 			}
+			return stringBuilder.toString();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return null;
 		}
-		try {
-			reader.close();
-		} catch (IOException e) {			
-			e.printStackTrace();
-		}
-		return stringBuilder.toString();
 	}
 
 	public static void main(String[] args) throws IOException {
